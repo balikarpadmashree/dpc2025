83979cd75823021908f121c73db95392406e0a35
balikarpadmashree <balikarpadmashree@gmail.com>
2025-08-30 23:11:18 +0530

    Create day16.cpp
    day16

---
+++ day16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+long long gcd(long long a, long long b)
+{
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long n, long long m) 
+{
+    return (n / gcd(n, m)) * m; 
+}
+int main() {
+    long long N, M;
+    cin >> N >> M;
+    cout << lcm(N, M) << endl;
+    return 0;
+}
