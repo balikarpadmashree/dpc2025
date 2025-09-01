25bfe2d852a8198151a31839f94aa32da56e36ed
balikarpadmashree <balikarpadmashree@gmail.com>
2025-09-01 13:33:48 +0530

    Create day18.cpp
    day18

---
+++ day18.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int countDivisors(int N) {
+    int count = 0;
+    for (int i = 1; i * 1LL * i <= N; i++) {
+        if (N % i == 0) {
+            if (i == N / i)
+                count += 1;
+            else
+                count += 2;
+        }
+    }
+    return count;
+}
+int main() {
+    int N;
+    cin >> N;
+    cout << countDivisors(N) << endl;
+    return 0;
+}
